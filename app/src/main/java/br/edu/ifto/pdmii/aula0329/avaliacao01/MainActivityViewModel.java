75939fe37d4bfcaf58acf84ba3994ed90bdeccc1
Victor Nunes <nunessvictorr@gmail.com>
2022-04-04 14:25:39 -0300

    Add initDatabase action

--- app/src/main/java/br/edu/ifto/pdmii/aula0329/avaliacao01/MainActivityViewModel.java
+++ app/src/main/java/br/edu/ifto/pdmii/aula0329/avaliacao01/MainActivityViewModel.java
@@ -1,7 +1,6 @@
 package br.edu.ifto.pdmii.aula0329.avaliacao01;
 
 import android.app.Application;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -9,106 +8,30 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.room.Room;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.Executors;
 
-import br.edu.ifto.pdmii.aula0329.avaliacao01.api.NamedApiResource;
-import br.edu.ifto.pdmii.aula0329.avaliacao01.api.NamedApiResourceList;
-import br.edu.ifto.pdmii.aula0329.avaliacao01.api.PokemonAbilityApiResource;
-import br.edu.ifto.pdmii.aula0329.avaliacao01.api.PokemonApiResource;
-import br.edu.ifto.pdmii.aula0329.avaliacao01.api.PokemonTypeApiResource;
-import br.edu.ifto.pdmii.aula0329.avaliacao01.model.Ability;
-import br.edu.ifto.pdmii.aula0329.avaliacao01.model.Pokemon;
 import br.edu.ifto.pdmii.aula0329.avaliacao01.model.dao.AppDatabase;
 import br.edu.ifto.pdmii.aula0329.avaliacao01.repository.PokemonRepository;
 import br.edu.ifto.pdmii.aula0329.avaliacao01.repository.Result;
 
 public class MainActivityViewModel extends AndroidViewModel {
-    private final PokemonRepository pokemonRepository;
     private final MutableLiveData<Boolean> mIsDatabaseInitilized = new MutableLiveData<>(false);
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
-        this.pokemonRepository = new PokemonRepository(Executors.newSingleThreadExecutor());
-        makeFetchNamedApiResourceListRequest();
-    }
 
-    private void makeFetchNamedApiResourceListRequest() {
-        pokemonRepository.makeFetchNamedApiRequest(result -> {
+        AppDatabase database = Room.databaseBuilder(
+                getApplication().getApplicationContext(),
+                AppDatabase.class, "pokemon-db").build();
+
+        PokemonRepository pokemonRepository = new PokemonRepository(
+                database, Executors.newSingleThreadExecutor());
+
+        pokemonRepository.initDatabase(result -> {
             if (result instanceof Result.Success) {
-                NamedApiResourceList resourceList = ((Result.Success<NamedApiResourceList>) result).data;
-
-                for (NamedApiResource resource : resourceList.getResults()) {
-                    Log.d(MainActivityViewModel.class.getName(), "Fetching " + resource.getUrl());
-                    makeFetchPokemonRequest(resource.getUrl());
-                }
-
                 mIsDatabaseInitilized.postValue(true);
             }
         });
-    }
-
-    private void makeFetchPokemonRequest(String url) {
-        pokemonRepository.makeFetchPokemonRequest(url, result -> {
-            if (result instanceof Result.Success) {
-                PokemonApiResource pokemonApiResource = ((Result.Success<PokemonApiResource>) result).data;
-                Log.d(MainActivityViewModel.class.getName(), "Fetched " + pokemonApiResource.name);
-
-                Pokemon pokemon = createPokemonFromApiResource(pokemonApiResource);
-                List<Ability> abilities = createAbilitiesFromApiResource(pokemonApiResource);
-
-                Log.d(MainActivityViewModel.class.getName(), "Inserting " + pokemon.name);
-                makeDatabasePokemonInsert(pokemon, abilities);
-            }
-        });
-    }
-
-    private void makeDatabasePokemonInsert(Pokemon pokemon, List<Ability> abilities) {
-        AppDatabase db = Room.databaseBuilder(getApplication().getApplicationContext(),
-                AppDatabase.class, "pokemon-db").build();
-
-        pokemonRepository.makeDatabasePokemonInsert(db, pokemon, abilities, result -> {
-            if (result instanceof Result.Success) {
-                Log.d(MainActivityViewModel.class.getName(), "Inserted " + pokemon.name);
-            }
-        });
-    }
-
-    private Pokemon createPokemonFromApiResource(PokemonApiResource resource) {
-        Pokemon pokemon = new Pokemon();
-        List<String> types = new ArrayList<>();
-
-        pokemon.pokemonId = resource.id;
-        pokemon.name = resource.name;
-        pokemon.baseExperience = resource.baseExperience;
-        pokemon.height = resource.height;
-        pokemon.weight = resource.weight;
-        pokemon.sprites = resource.sprites;
-
-        for (PokemonTypeApiResource typeApiResource : resource.types) {
-            types.add(typeApiResource.getType().getName());
-        }
-
-        pokemon.types = types;
-
-        return pokemon;
-    }
-
-    private List<Ability> createAbilitiesFromApiResource(PokemonApiResource resource) {
-        List<PokemonAbilityApiResource> abilityApiResources = resource.abilities;
-        List<Ability> abilities = new ArrayList<>();
-
-        for (PokemonAbilityApiResource abilityApiResource : abilityApiResources) {
-            Ability ability = new Ability();
-
-            ability.isHidden = abilityApiResource.isHidden;
-            ability.ability = abilityApiResource.ability.getName();
-
-            abilities.add(ability);
-        }
-
-        return abilities;
     }
 
     public LiveData<Boolean> isDatabaseInitilized() {
--- app/src/main/java/br/edu/ifto/pdmii/aula0329/avaliacao01/repository/PokemonRepository.java
+++ app/src/main/java/br/edu/ifto/pdmii/aula0329/avaliacao01/repository/PokemonRepository.java
@@ -8,11 +8,15 @@ import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 
+import br.edu.ifto.pdmii.aula0329.avaliacao01.api.NamedApiResource;
 import br.edu.ifto.pdmii.aula0329.avaliacao01.api.NamedApiResourceList;
+import br.edu.ifto.pdmii.aula0329.avaliacao01.api.PokemonAbilityApiResource;
 import br.edu.ifto.pdmii.aula0329.avaliacao01.api.PokemonApiResource;
+import br.edu.ifto.pdmii.aula0329.avaliacao01.api.PokemonTypeApiResource;
 import br.edu.ifto.pdmii.aula0329.avaliacao01.http.HttpClient;
 import br.edu.ifto.pdmii.aula0329.avaliacao01.model.Ability;
 import br.edu.ifto.pdmii.aula0329.avaliacao01.model.Pokemon;
@@ -25,51 +29,39 @@ import br.edu.ifto.pdmii.aula0329.avaliacao01.util.RandomInt;
 
 public class PokemonRepository {
     private final Executor executor;
+    private final AppDatabase database;
 
-    public PokemonRepository(Executor executor) {
+    public PokemonRepository(AppDatabase database, Executor executor) {
+        this.database = database;
         this.executor = executor;
     }
 
-    public void makeFetchNamedApiRequest(
-            final RepositoryCallback<NamedApiResourceList> callback) {
-
+    public void initDatabase(final RepositoryCallback<Void> callback) {
         executor.execute(() -> {
-            try {
-                Result<NamedApiResourceList> result = makeSynchronousFetchNamedApiRequest();
-                callback.onComplete(result);
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (database.pokemonDao().getAll().size() == 0) {
+                try {
+                    NamedApiResourceList result = makeSynchronousFetchNamedApiRequest();
+
+                    for (NamedApiResource namedApiResource : result.getResults()) {
+                        PokemonApiResource pokemonApiResource = makeSynchronousFetchPokemonRequest(
+                                namedApiResource.getUrl());
+                        Pokemon pokemon = createPokemonFromApiResource(pokemonApiResource);
+                        List<Ability> abilities = createAbilitiesFromApiResource(pokemonApiResource);
+                        makeSynchronousDatabasePokemonInsert(pokemon, abilities);
+                    }
+
+                    callback.onComplete(new Result.Success<>(null));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    callback.onComplete(new Result.Error<>(e));
+                }
+            } else {
+                callback.onComplete(new Result.Success<>(null));
             }
         });
     }
 
-    public void makeFetchPokemonRequest(
-            final String url,
-            final RepositoryCallback<PokemonApiResource> callback) {
-
-        executor.execute(() -> {
-            try {
-                Result<PokemonApiResource> result = makeSynchronousFetchPokemonRequest(url);
-                callback.onComplete(result);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
-    public void makeDatabasePokemonInsert(
-            final AppDatabase db,
-            final Pokemon pokemon,
-            final List<Ability> abilities,
-            final RepositoryCallback<Void> callback) {
-
-        executor.execute(() -> {
-            Result<Void> result = makeSynchronousDatabasePokemonInsert(db, pokemon, abilities);
-            callback.onComplete(result);
-        });
-    }
-
-    private Result<NamedApiResourceList> makeSynchronousFetchNamedApiRequest() throws IOException {
+    private NamedApiResourceList makeSynchronousFetchNamedApiRequest() throws IOException {
         int limit = RandomInt.between(10, 20);
         int offset = RandomInt.between(0, 1126 - limit);
 
@@ -83,25 +75,25 @@ public class PokemonRepository {
         Type type = new TypeToken<NamedApiResourceList>() {
         }.getType();
 
-        return new Result.Success<>(gson.fromJson(data, type));
+        return gson.fromJson(data, type);
     }
 
-    private Result<PokemonApiResource> makeSynchronousFetchPokemonRequest(String url) throws IOException {
+    private PokemonApiResource makeSynchronousFetchPokemonRequest(String url) throws IOException {
         Gson gson = new Gson();
 
         String data = new HttpClient().get(url);
         Type type = new TypeToken<PokemonApiResource>() {
         }.getType();
 
-        return new Result.Success<>(gson.fromJson(data, type));
+        return gson.fromJson(data, type);
     }
 
-    private Result<Void> makeSynchronousDatabasePokemonInsert(
-            AppDatabase db, Pokemon pokemon, List<Ability> abilities) {
+    private void makeSynchronousDatabasePokemonInsert(
+            Pokemon pokemon, List<Ability> abilities) {
 
-        PokemonDao pokemonDao = db.pokemonDao();
-        AbilityDao abilityDao = db.abilityDao();
-        PokemonAbilityDao pokemonAbilityDao = db.pokemonAbilityDao();
+        PokemonDao pokemonDao = database.pokemonDao();
+        AbilityDao abilityDao = database.abilityDao();
+        PokemonAbilityDao pokemonAbilityDao = database.pokemonAbilityDao();
 
         long pokemonId = pokemonDao.insert(pokemon);
         long[] abilitiesIds = abilityDao.insertAll(abilities.toArray(new Ability[]{}));
@@ -114,7 +106,41 @@ public class PokemonRepository {
 
             pokemonAbilityDao.insert(pokemonAbility);
         }
+    }
 
-        return new Result.Success<>(null);
+    private Pokemon createPokemonFromApiResource(PokemonApiResource resource) {
+        Pokemon pokemon = new Pokemon();
+        List<String> types = new ArrayList<>();
+
+        pokemon.pokemonId = resource.id;
+        pokemon.name = resource.name;
+        pokemon.baseExperience = resource.baseExperience;
+        pokemon.height = resource.height;
+        pokemon.weight = resource.weight;
+        pokemon.sprites = resource.sprites;
+
+        for (PokemonTypeApiResource typeApiResource : resource.types) {
+            types.add(typeApiResource.getType().getName());
+        }
+
+        pokemon.types = types;
+
+        return pokemon;
+    }
+
+    private List<Ability> createAbilitiesFromApiResource(PokemonApiResource resource) {
+        List<PokemonAbilityApiResource> abilityApiResources = resource.abilities;
+        List<Ability> abilities = new ArrayList<>();
+
+        for (PokemonAbilityApiResource abilityApiResource : abilityApiResources) {
+            Ability ability = new Ability();
+
+            ability.isHidden = abilityApiResource.isHidden;
+            ability.ability = abilityApiResource.ability.getName();
+
+            abilities.add(ability);
+        }
+
+        return abilities;
     }
 }
