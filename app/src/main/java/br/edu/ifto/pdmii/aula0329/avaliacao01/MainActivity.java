0b519c7c372e53b584c24e15c3c6834fe5899260
Victor Nunes <nunessvictorr@gmail.com>
2022-04-04 14:30:44 -0300

    Code refactoring

--- app/src/main/java/br/edu/ifto/pdmii/aula0329/avaliacao01/MainActivity.java
+++ app/src/main/java/br/edu/ifto/pdmii/aula0329/avaliacao01/MainActivity.java
@@ -1,7 +1,6 @@
 package br.edu.ifto.pdmii.aula0329.avaliacao01;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -27,35 +26,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Remove database on app init
         deleteDatabase("pokemon-db");
         MainActivityViewModel viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
         viewModel.isDatabaseInitilized().observe(this, isDatabaseInitilized -> {
             if (!isDatabaseInitilized) {
+                // Start pokeball spinner view
                 setContentView(R.layout.content_main_loader);
                 return;
             }
 
-            ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-            setContentView(binding.getRoot());
-
-            setSupportActionBar(binding.appBarMain.toolbar);
-            binding.appBarMain.fab.setOnClickListener(view ->
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show());
-
-            DrawerLayout drawer = binding.drawerLayout;
-            NavigationView navigationView = binding.navView;
-
-            mAppBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.nav_description, R.id.nav_details, R.id.nav_sizes, R.id.nav_play)
-                    .setOpenableLayout(drawer)
-                    .build();
-
-            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-            NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-            NavigationUI.setupWithNavController(navigationView, navController);
+            // Start default view
+            initView();
         });
+    }
+
+    private void initView() {
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show());
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_description, R.id.nav_details, R.id.nav_sizes, R.id.nav_play)
+                .setOpenableLayout(drawer)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
